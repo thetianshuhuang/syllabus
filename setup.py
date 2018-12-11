4fcdc48e2398ec181307cdc83805ce42ab6140f7
thetianshuhuang <thetianshuhuang@gmail.com>
2018-12-10 21:50:05 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(
+    name='Syllabus',
+    version='1.0',
+    packages=['syllabus'],
+    license='MIT',
+    author='Tianshu Huang',
+    author_email='thetianshuhuang@gmail.com',
+)
