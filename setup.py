cf7dc5c59e6d49788c63edfc4e334daaad806749
Tianshu Huang <thetianshuhuang@gmail.com>
2019-03-27 14:07:52 -0500

    Updated setup.py for full distribution

--- setup.py
+++ setup.py
@@ -1,11 +1,33 @@
 
-from distutils.core import setup
+from setuptools import setup
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='Syllabus',
     version='1.1',
-    packages=['syllabus', 'syllabus.task'],
+    packages=[
+        'syllabus',
+        'syllabus.format',
+        'syllabus.app_utils',
+        'syllabus.parallel',
+        'syllabus.reporting'
+    ],
+    install_requires=['pyfiglet', 'ansiwrap'],
     license='MIT',
     author='Tianshu Huang',
     author_email='thetianshuhuang@gmail.com',
+    description=(
+        'Time and memory tracking task manager for Machine Learning '
+        'applications'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/thetianshuhuang/syllabus',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
 )
